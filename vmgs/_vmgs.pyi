a57b56848a1993fc2a8ad8bb3210ccdda5861e50
HyperSine <hypersine.git@outlook.com>
2023-05-02 14:49:49 +0800

    fix typing
    Signed-off-by: HyperSine <hypersine.git@outlook.com>

--- vmgs/_vmgs.pyi
+++ vmgs/_vmgs.pyi
@@ -3,10 +3,10 @@ class VmgsIO:
     def __init__(self, **kwargs):
         pass
 
-    def __enter__(self):
+    def __enter__(self) -> VmgsIO:
         pass
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
         pass
 
     def read(self) -> bytes:
